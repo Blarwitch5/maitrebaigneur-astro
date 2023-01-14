844ea83337e2eedff4d320f6090a09af88dcb1ef
Romain Blary <romain.blary.uni@gmail.com>
2023-01-14 13:49:15 +0100

    Ajout des fichier de config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,18 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	server: { port: 8080 },
+	output: "static",
+	vite: {
+		build: {
+			rollupOptions: {
+				output: {
+					assetFileNames: "assets/styles/[name][extname]",
+				},
+			},
+		},
+	},
+	//changed because conflict in local with Local by Flywheel for port 3000
+	server: {
+		port: 8080,
+	},
 });
