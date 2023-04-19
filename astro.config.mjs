9b79a4190f21e227e26d10d47b661b3d0073e6b0
Romain Blary <romain.blary.uni@gmail.com>
2023-04-19 10:48:05 +0200

    Fix: résolution du problème des canonical, ajout de l'URL du site (à changer si le domaine change git add --allgit add --all) et appel en JS avec Astro.site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,23 +3,24 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	output: "static",
-	vite: {
-		build: {
-			rollupOptions: {
-				output: {
-					assetFileNames: "assets/styles/[name][extname]",
-				},
-			},
-		},
-	},
-	//changed because conflict in local with Local by Flywheel for port 3000
-	server: {
-		port: 8080,
-	},
-	integrations: [
-		// image({
-		// 	serviceEntryPoint: "@astrojs/image/sharp",
-		// }),
-	],
+  site: "https://maitrebaigneur.netlify.app/contact/",
+  output: "static",
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: "assets/styles/[name][extname]",
+        },
+      },
+    },
+  },
+  //changed because conflict in local with Local by Flywheel for port 3000
+  server: {
+    port: 8080,
+  },
+  integrations: [
+    // image({
+    // 	serviceEntryPoint: "@astrojs/image/sharp",
+    // }),
+  ],
 });
