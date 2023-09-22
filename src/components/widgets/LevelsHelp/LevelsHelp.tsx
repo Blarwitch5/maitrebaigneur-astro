7e2c6bbc1597492d7c647c459d25ac15b40ce116
Romain Blary <romain.blary.uni@gmail.com>
2023-09-22 19:07:02 +0200

    Dev:mise en place d'une modal pour afficher une aide au niveaux de natation pour libérer le formulaire, reste à finaliser le style

---
+++ src/components/widgets/LevelsHelp/LevelsHelp.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { useEffect, useRef } from "react";
+
+import "./LevelsHelp.scss";
+
+interface LevelsHelpProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const LevelsHelp: React.FC<React.PropsWithChildren<LevelsHelpProps>> = ({
+  isOpen,
+  onClose,
+  children,
+}) => {
+  const popupRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (isOpen && popupRef.current) {
+      popupRef.current.focus();
+    }
+  }, [isOpen]);
+
+  return (
+    <div
+      className="dialog"
+      role="dialog"
+      aria-modal="true"
+      aria-labelledby="dialog-title"
+      aria-describedby="dialog-description"
+    >
+      {isOpen && (
+        <>
+          <div className="dialog-overlay" aria-hidden="true"></div>
+          <div className="dialog-content">
+            <button onClick={onClose} className="btn-back ">
+              Retour au formulaire
+            </button>
+            <div className="scrollable-content">{children}</div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default LevelsHelp;
