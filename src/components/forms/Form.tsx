f7421d8950bf40364de93c9c0f2bd2da48b65e42
Romain Blary <romain.blary.uni@gmail.com>
2023-09-26 15:08:39 +0200

    dev: mise en place des options dans un fichier json externe

--- src/components/forms/Form.tsx
+++ src/components/forms/Form.tsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from "react";
-import OptionsRadioBtn from "@components/forms/OptionsRadioBtn";
+import React, { useState, useEffect } from "react";
 
+import OptionsRadioBtn from "@components/forms/OptionsRadioBtn";
 import SwimmingForm from "@components/forms/SwimmingForm";
 import AquaversaryForm from "@components/forms/AquaversaryForm";
 import SurveillanceForm from "@components/forms/SurveillanceForm";
@@ -8,6 +8,10 @@ import ContactForm from "@components/forms/ContactForm";
 import BabySwimmerForm from "@components/forms/BabySwimmerForm";
 import SwimminginstationForm from "@components/forms/SwimmingInStationForm";
 
+// Import the options data from the JSON file
+import optionsData from "../../assets/data/formOptions.json";
+
+// Define the Option type
 interface Option {
   option: string;
   value: string;
@@ -15,93 +19,53 @@ interface Option {
   title: string;
   content: string;
   className: string;
-  formComponent: React.FC;
+  formComponent: string; // Change this to string
 }
 
-const options: Option[] = [
-  {
-    option: "option1",
-    value: "leçons de natation en station",
-    slug: "lecons-de-natation-en-station",
-    title: "Leçons de natation en bassin d'apprentissage",
-    content: "Custom content for option 1",
-    className: "card card-swimming",
-    formComponent: SwimminginstationForm,
-  },
-  {
-    option: "option2",
-    value: "Cours de bébé nageur",
-    slug: "cours-de-bebe-nageur",
-    title: "Cours de bébé nageur",
-    content: "Custom content for option 2",
-    className: "card card-baby",
-    formComponent: BabySwimmerForm,
-  },
-  {
-    option: "option3",
-    value: "leçons de natation à domicile",
-    slug: "lecons-de-natation-a-domicile",
-    title: "Leçons de natation à domicile",
-    content: "Custom content for option 3",
-    className: "card card-swimming",
-    formComponent: SwimmingForm,
-  },
-  {
-    option: "option4",
-    value: "Sécurité d'un événement",
-    slug: "securiser-un-evenement",
-    title: "Sécurité d'un événement",
-    content: "Custom content for option 4",
-    className: "card card-events",
-    formComponent: SurveillanceForm,
-  },
-  {
-    option: "option5",
-    value: "Organisation d'un aquaversaire",
-    slug: "organiser-un-aquaversaire",
-    title: "Organisation d'un aquaversaire",
-    content: "Custom content for option 5",
-    className: "card card-aquaversary",
-    formComponent: AquaversaryForm,
-  },
-  {
-    option: "option6",
-    value: "Autres questions ou suggestions",
-    slug: "autres-questions-ou-suggestions",
-    title: "Autres questions ou suggestions",
-    content: "Custom content for option 6",
-    className: "card card-questions",
-    formComponent: ContactForm,
-  },
-];
 const Form = () => {
   const [selectedOption, setSelectedOption] = useState<string>("");
 
   const updateURL = (slug: string) => {
     const newUrl = `/contact?option=${slug}`;
     window.history.pushState({}, "", newUrl);
-
-    // After updating the URL, set the selected option
     setSelectedOption(slug);
   };
 
   useEffect(() => {
-    // Parse the URL to get the 'option' query parameter
     const urlParams = new URLSearchParams(window.location.search);
     const optionFromURL = urlParams.get("option");
 
     if (optionFromURL) {
-      // Set the selected option if the 'option' query parameter is present
       setSelectedOption(optionFromURL);
     }
   }, []);
+
+  // Define the type for renderFormComponent
+  const renderFormComponent = (componentName: string): JSX.Element | null => {
+    switch (componentName) {
+      case "SwimminginstationForm":
+        return <SwimminginstationForm />;
+      case "BabySwimmerForm":
+        return <BabySwimmerForm />;
+      case "SwimmingForm":
+        return <SwimmingForm />;
+      case "SurveillanceForm":
+        return <SurveillanceForm />;
+      case "AquaversaryForm":
+        return <AquaversaryForm />;
+      case "ContactForm":
+        return <ContactForm />;
+      default:
+        return null;
+    }
+  };
 
   return (
     <>
       <fieldset className="form__group">
         <legend>En quoi pouvons-nous vous aider ?</legend>
         <div className="radio__group">
-          {options.map((option) => (
+          {optionsData.map((option: Option) => (
             <div className="form__field" key={option.option}>
               <OptionsRadioBtn
                 option={option}
@@ -115,11 +79,11 @@ const Form = () => {
         </div>
       </fieldset>
 
-      {options
-        .filter((option) => selectedOption === option.slug)
-        .map((option) => (
+      {optionsData
+        .filter((option: Option) => selectedOption === option.slug)
+        .map((option: Option) => (
           <div key={option.slug}>
-            <option.formComponent />
+            {renderFormComponent(option.formComponent)}
           </div>
         ))}
     </>
