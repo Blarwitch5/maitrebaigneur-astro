b9a963f93d8d257f87cb3ab3fc5c643e976c8055
Romain Blary <romain.blary.uni@gmail.com>
2024-03-15 09:10:24 +0100

    Maj de l'URL du site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://maitrebaigneur.vercel.app",
+	site: "https://maitrebaigneur.com",
 	vite: {
 		build: {
 			rollupOptions: {
