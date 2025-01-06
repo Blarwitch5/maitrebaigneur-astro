2cbc679c294ff1ed979dafc494c1e4553b003497
Romain Blary <romain.blary.uni@gmail.com>
2025-01-06 15:33:24 +0100

    mise à jour de Astro (contentCollection), packages, scss et url clic rdv

---
+++ src/content.config.ts
@@ -0,0 +1,43 @@
+import { defineCollection, z } from "astro:content";
+
+const poolsCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    metaTitle: z.string(),
+    metaDesc: z.string(),
+    title: z.string(),
+    pool: z.object({
+      name: z.string(),
+      location: z.string(),
+      desc: z.string(),
+      image: z.object({
+        webp: z.string(),
+        jpg: z.string(),
+        alt: z.string(),
+      }),
+      address: z.string(),
+      link: z.object({
+        url: z.string(),
+        text: z.string(),
+      }),
+      services: z.array(
+        z.object({
+          name: z.string(),
+          icon: z.string(),
+          href: z.string(),
+        })
+      ),
+      gallery: z.array(
+        z.object({
+          webp: z.string(),
+          jpg: z.string(),
+          alt: z.string(),
+        })
+      ),
+    }),
+  }),
+});
+
+export const collections = {
+  bassins: poolsCollection,
+};
