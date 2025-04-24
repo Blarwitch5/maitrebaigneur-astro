db45282bce046637ea3603f2b32dfaf8431c93c1
Romain Blary <romain.blary.uni@gmail.com>
2025-04-24 13:07:50 +0200

    Changement de système de réservation

--- src/components/forms/SwimmingInStationForm.tsx
+++ src/components/forms/SwimmingInStationForm.tsx
@@ -32,7 +32,7 @@ const SwimmingInStationForm = () => {
       <a
         className="btn btn__regular"
         target="_blank"
-        href="https://user.clicrdv.com/sarl-qfe"
+        href="https://maitrebaigneur.liberfit.fr/customportal/login#%0A"
         rel="noopener noreferrer"
       >
         Je réserve maintenant
