6c40c2ca43e9b05cf7cf3c7805753cc3b27c8075
Romain Blary <romain.blary.uni@gmail.com>
2025-07-09 17:53:24 +0200

    Correction Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
+import vercel from '@astrojs/vercel';
+
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://maitrebaigneur.com",
+	adapter: vercel(),
 	vite: {
 		build: {
 			rollupOptions: {
