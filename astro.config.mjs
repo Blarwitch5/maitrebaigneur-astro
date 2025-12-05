29064bf633fed3fe8ee4655b1e7603285c152f07
Romain Blary <romain.blary.uni@gmail.com>
2025-12-05 19:16:17 +0100

    chore: mise à jour de la configuration Astro et dépendances
    - Mise à jour des fichiers de configuration Astro (.astro/*)
    - Mise à jour de astro.config.mjs
    - Mise à jour de package.json et pnpm-lock.yaml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-import vercel from '@astrojs/vercel';
-
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
@@ -19,7 +18,7 @@ export default defineConfig({
 	},
 	//changed because conflict in local with Local by Flywheel for port 3000
 	server: {
-		port: 8080,
+		port: 3000,
 	},
 	integrations: [
 		react(),
